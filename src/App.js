0151bfbc9b15edc45f070cc32c3dc522d63ad2fe
mojojojo244 <jordan.crutcher@gmail.com>
2022-02-02 13:22:39 -0600

    basic functionality with json server

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NotFound from './NotFound';
 
 function App() {
   return (
-    <div className='App'>
-      <Navbar />
-      <div className='content'>
-        <Home />
+    <Router>
+      <div className='App'>
+        <Navbar />
+        <div className='content'>
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route path='/create'>
+              <Create />
+            </Route>
+            <Route path='/blogs/:id'>
+              <BlogDetails />
+            </Route>
+            <Route path='*'>
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
